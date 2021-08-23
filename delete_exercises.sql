4471ff6cb6c6b68d3b709b7101da3431720a5479
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-23 15:44:05 -0500

    Deleted albums newer than 1991, disco and Whitney Houston

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,13 +1,10 @@
 use codeup_test_db;
 
-select name as '-------Albums Released After 1991-------'
-from albums
+delete from albums
 where release_date > 1991;
 
-select name as '-------Disco Albums-------'
-from albums
+delete from albums
 where genre = 'disco';
 
-select name as '-------Whitney Houston Albums-------'
-from albums
+delete from albums
 where artist = 'Whitney Houston';
