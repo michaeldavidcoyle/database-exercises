95a0762900e71a1e3c3b2f2a557a0ee69441f57c
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-24 13:53:21 -0500

    Getting 10th set or page of results of previous query

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -13,3 +13,10 @@ SELECT emp_no
 FROM salaries
 ORDER BY salary DESC
 LIMIT 5;
+
+# Try to think of your results as batches, sets, or pages. The first five results are your first page.
+# The five after that would be your second page, etc. Update the previous query to find the tenth page of results.
+SELECT emp_no
+FROM salaries
+ORDER BY salary DESC
+LIMIT 5 OFFSET 45;
