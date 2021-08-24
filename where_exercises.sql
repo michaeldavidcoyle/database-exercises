f95b5c99c870eedac053775bd42ae94ffb4bf732
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-24 11:26:52 -0500

    Selecting employees with a 'q' in their last name but not 'qu'

--- where_exercises.sql
+++ where_exercises.sql
@@ -43,3 +43,9 @@ SELECT *
 FROM employees
 WHERE last_name LIKE 'E%'
    AND last_name LIKE '%E';
+
+# Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%';
