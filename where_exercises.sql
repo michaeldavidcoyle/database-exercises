fc4e6c487af41bc96ae66ebc657ad3f60c3fa21d
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-24 11:00:45 -0500

    Create file for where clause exercises and selecting employees with given first names

---
+++ where_exercises.sql
@@ -0,0 +1,6 @@
+USE employees;
+
+# Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
