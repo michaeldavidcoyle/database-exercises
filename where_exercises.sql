d8bc735ed042e1d65ac8d746d60948f9ecd2eabb
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-24 11:05:03 -0500

    Selecting employees with a 'q' in their last name.

--- where_exercises.sql
+++ where_exercises.sql
@@ -9,3 +9,8 @@ WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 SELECT *
 FROM employees
 WHERE last_name LIKE 'E%';
+
+# Find all employees with a 'q' in their last name — 1,873 rows.
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
