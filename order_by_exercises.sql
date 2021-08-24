1df7314cb20189f1ad56542d853b6a9f56f8b768
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-24 13:32:50 -0500

    Completed order by exercises for first query, sorting by first name, then first and last name and finally by last name then first

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,9 +1,16 @@
 USE employees;
 
 # Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+# Modify your first query to order by first name. The first result should be Irena Reutenauer and the last result should be Vidya Demeyer.
+# Update the query to order by first name and then last name. The first result should now be Irena Acton and the last should be Vidya Boguraev.
+# Change the ORDER BY clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Vidya Quittner.
 SELECT *
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name;
+# Contrary to what the exercise says, first result is Irena Bisiani, last is Vidya Peac when ordering by first name.
+# Order by first and last name gives Irena Acton first but Vidya Zweizig last.
+# Ordering by last name and then first name still returns Irena Acton first but Maya Zyda last.
 
 # Find all employees whose last name starts with 'E' — 7,330 rows.
 SELECT *
