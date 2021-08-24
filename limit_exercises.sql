0edcb74c3a52b298f03016f05e144d990ffe582c
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-24 13:45:02 -0500

    Getting emp_no of top 5 salaries from the salaries table.

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -7,3 +7,9 @@ SELECT DISTINCT last_name
 FROM employees
 ORDER BY last_name DESC
 LIMIT 10;
+
+# Create a query to get the top 5 salaries and display just the employees number from the salaries table.
+SELECT emp_no
+FROM salaries
+ORDER BY salary DESC
+LIMIT 5;
