cc325c1fb76867af34640371c9765cf734046add
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-25 09:22:54 -0500

    Found all employees hired in the 90s with Dec 25th birthdays.

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -12,3 +12,10 @@ SELECT CONCAT(e.first_name, ' ', e.last_name) AS Name, e.birth_date AS Birthday
 FROM employees.employees e
 WHERE MONTH(e.birth_date) = 12
   AND DAY(e.birth_date) = 25;
+
+# Find all employees hired in the 90s and born on Christmas — 362 rows.
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS Name, e.birth_date AS Birthday, e.hire_date AS Hired
+FROM employees.employees e
+WHERE MONTH(e.birth_date) = 12
+  AND DAY(e.birth_date) = 25
+  AND YEAR(e.hire_date) BETWEEN 1990 AND 1999;
