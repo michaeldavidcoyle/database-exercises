788088c6c134c191a3c9e714ebf25268f73be9b3
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-25 09:27:26 -0500

    Same as previous query but ordered so the first result

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -19,3 +19,12 @@ FROM employees.employees e
 WHERE MONTH(e.birth_date) = 12
   AND DAY(e.birth_date) = 25
   AND YEAR(e.hire_date) BETWEEN 1990 AND 1999;
+
+# Change the query for employees hired in the 90s and born on Christmas such that the first result
+# is the oldest employee who was hired last. It should be Khun Bernini.
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS Name, e.birth_date AS Birthday, e.hire_date AS Hired
+FROM employees.employees e
+WHERE MONTH(e.birth_date) = 12
+  AND DAY(e.birth_date) = 25
+  AND YEAR(e.hire_date) BETWEEN 1990 AND 1999
+ORDER BY e.birth_date, e.hire_date DESC;
