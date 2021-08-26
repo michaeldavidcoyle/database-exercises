2d2347e7740e3aed1de7e93eb9318f2871b2b996
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-26 10:30:40 -0500

    Found unique last names with 'q' but not 'qu'.

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -13,3 +13,10 @@ select concat(e.last_name, ', ', e.first_name) full_name
 from employees.employees e
 where e.last_name like 'e%e'
 group by e.last_name, e.first_name;
+
+# Find the unique last names with a 'q' but not 'qu'.
+select e.last_name
+from employees.employees e
+where e.last_name like '%q%'
+  and e.last_name not like '%qu%'
+group by e.last_name;
