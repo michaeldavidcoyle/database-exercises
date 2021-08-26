e000a67470702f4c37d4a3108a791cf3be30589e
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-26 10:27:31 -0500

    Finding unique combinations of first and last name where the last name starts and ends with 'E'.

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -6,3 +6,10 @@ select e.last_name
 from employees.employees e
 where e.last_name like 'e%e'
 group by e.last_name;
+
+# Update your previous query to now find unique combinations of first and last name where the last name starts and ends
+# with 'E'. You should get 846 rows.
+select concat(e.last_name, ', ', e.first_name) full_name
+from employees.employees e
+where e.last_name like 'e%e'
+group by e.last_name, e.first_name;
