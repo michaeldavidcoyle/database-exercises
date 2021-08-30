99bbfe305d046e89ee116071df3aa08a8340b5c9
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-30 12:05:27 -0500

    Found all titles held by employees with the first name 'Aamod'.

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -8,3 +8,13 @@ where e.hire_date in (
     from employees.employees e
     where e.emp_no = 101010
 );
+
+# Find all the titles held by all employees with the first name Aamod.
+# 314 total titles, 6 unique titles
+select t.title Title
+from titles t
+where emp_no in (
+    select emp_no
+    from employees.employees e
+    where e.first_name = 'Aamod'
+);
