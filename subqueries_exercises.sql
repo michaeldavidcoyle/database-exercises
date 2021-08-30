f676881e8b0ac8454edca6fae5e5188697002a8d
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-30 12:00:32 -0500

    Found all employees with the same hire date as employee 101010.

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,1 +1,10 @@
 use employees;
+
+# Find all the employees with the same hire date as employee 101010 using a subquery. 69 Rows
+select *
+from employees.employees e
+where e.hire_date in (
+    select e.hire_date
+    from employees.employees e
+    where e.emp_no = 101010
+);
